777ceb43c1961bd52b47f0e86a2b9e29be80d22e
longxingyun686 <longxingyun686@gmail.com>
2021-11-06 22:21:50 +0800

    master

---
+++ cpp/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+int main() {
+    cout << "hi, I love you!" << endl;
+    vector<int> a;
+    a.push_back(5);
+    cout << a[0] << endl;
+    system("pause");
+    return 0;
+}
+
